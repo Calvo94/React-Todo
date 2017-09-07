eb081c97279b3cc5773642bfe068e77ef8bdf605
unknown <Saiidma@gmail.com>
2017-09-07 23:06:03 +0100

    Complete handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TodoList from 'TodoList';
 import TodoForm from 'TodoForm';
 import TodoSearch from 'TodoSearch';
+import uuid from 'node-uuid';
 
 class TodoApp extends Component {
   state={
@@ -9,19 +10,19 @@ class TodoApp extends Component {
     searchText:'',
     todos: [
       {
-        id:1,
+        id:uuid(),
         text: 'Walk the dog'
       },
       {
-        id:2,
+        id:uuid(),
         text: 'Clean the yard'
       },
       {
-        id:3,
+        id:uuid(),
         text: 'Leave mail on porch'
       },
       {
-        id:4,
+        id:uuid(),
         text: 'Play video games'
       },
     ]
@@ -33,7 +34,15 @@ class TodoApp extends Component {
     })
   }
   handleAddTodo(text) {
-    alert('new todo: '+text);
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text:text
+        }
+      ]
+    })
   }
 
   render() {
--- app/tests/Components/TodoApp.test.jsx
+++ app/tests/Components/TodoApp.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jQuery';
+import TestUtils from 'react-addons-test-utils';
 import TodoApp from 'TodoApp';
 
 var expect= require('expect');
@@ -9,4 +10,14 @@ describe('TodoApp', () => {
   it('should exist', () => {
       expect(TodoApp).toExist();
   });
+
+  it('should add todo to the todos state on ',() => {
+    var todoText = 'test text';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  })
 });
